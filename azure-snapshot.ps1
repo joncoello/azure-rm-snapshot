4a05709eb24754d4a978ecac778988bf0e6a694a
Jonathan Coello <Jonathan Coello>
2016-07-25 07:45:17 +0100

    second commit

---
+++ azure-snapshot.ps1
@@ -0,0 +1,71 @@
+﻿# Set variable values
+$resourceGroupName = "WF"
+$location = "North Europe"
+$vmName = "WkWF"
+$vmSize = "Basic_A2"
+$vnetName = "WF"
+$nicName = "wkwf99"
+$dnsName = "wkwf"
+$diskName = "WkWF2016624174911"
+$storageAccount = "wf4964"
+$storageAccountKey = "S1fjMg7J2JCPeJdYw/JY6y9rZ9DCN5KBAJ8QHrMv8KUZVsUXVGwMOczdiHiP9IyVRV2d4x28xuFpRGLsri4ZHg=="
+$subscriptionName = "Jon Coello WK"
+$publicIpName = "WkWF"
+$nsgID = "/subscriptions/956ce54c-a40b-4895-b229-dc75174b0787/resourceGroups/WF/providers/Microsoft.Network/networkSecurityGroups/WkWF"
+
+$diskBlob = "$diskName.vhd"
+$backupDiskBlob = "$diskName-backup.vhd"
+$vhdUri = "https://$storageAccount.blob.core.windows.net/vhds/$diskBlob"
+$subnetIndex = 0
+
+# login to Azure
+Add-AzureRmAccount
+Set-AzureRMContext -SubscriptionName $subscriptionName
+
+# create backup disk if it doesn't exist
+Stop-AzureRmVM -ResourceGroupName $resourceGroupName -Name $vmName -Force -Verbose
+
+$ctx = New-AzureStorageContext -StorageAccountName $storageAccount -StorageAccountKey $storageAccountKey
+$blobCount = Get-AzureStorageBlob -Container vhds -Context $ctx | where { $_.Name -eq $backupDiskBlob } | Measure | % { $_.Count }
+
+if ($blobCount -eq 0)
+{
+  $copy = Start-AzureStorageBlobCopy -SrcBlob $diskBlob -SrcContainer "vhds" -DestBlob $backupDiskBlob -DestContainer "vhds" -Context $ctx -Verbose
+  $status = $copy | Get-AzureStorageBlobCopyState 
+  $status 
+
+  While($status.Status -eq "Pending"){
+    $status = $copy | Get-AzureStorageBlobCopyState 
+    Start-Sleep 10
+    $status
+  }
+}
+
+# delete VM
+Remove-AzureRmVM -ResourceGroupName $resourceGroupName -Name $vmName -Force -Verbose
+Remove-AzureStorageBlob -Blob $diskBlob -Container "vhds" -Context $ctx -Verbose
+Remove-AzureRmNetworkInterface -Name $nicName -ResourceGroupName $resourceGroupName -Force -Verbose
+Remove-AzureRmPublicIpAddress -Name $publicIpName -ResourceGroupName $resourceGroupName -Force -Verbose
+
+# copy backup disk
+$copy = Start-AzureStorageBlobCopy -SrcBlob $backupDiskBlob -SrcContainer "vhds" -DestBlob $diskBlob -DestContainer "vhds" -Context $ctx -Verbose
+$status = $copy | Get-AzureStorageBlobCopyState 
+$status 
+
+While($status.Status -eq "Pending"){
+  $status = $copy | Get-AzureStorageBlobCopyState 
+  Start-Sleep 10
+  $status
+}
+
+
+# recreate VM
+$vnet = Get-AzureRmVirtualNetwork -Name $vnetName -ResourceGroupName $resourceGroupName
+
+$pip = New-AzureRmPublicIpAddress -Name $publicIpName -ResourceGroupName $resourceGroupName -DomainNameLabel $dnsName -Location $location -AllocationMethod Dynamic -Verbose
+$nic = New-AzureRmNetworkInterface -Name $nicName -ResourceGroupName $resourceGroupName -Location $location -SubnetId $vnet.Subnets[$subnetIndex].Id -PublicIpAddressId $pip.Id -NetworkSecurityGroupId $nsgID -Verbose
+$vm = New-AzureRmVMConfig -VMName $vmName -VMSize $vmSize
+$vm = Add-AzureRmVMNetworkInterface -VM $vm -Id $nic.Id
+$vm = Set-AzureRmVMOSDisk -VM $vm -Name $diskName -VhdUri $vhdUri -CreateOption attach -Windows
+
+New-AzureRmVM -ResourceGroupName $resourceGroupName -Location $location -VM $vm -Verbose
